4648d542b442e7230cc4720ded4e6b56ffb8afbd
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-11-16 10:15:02 -0500

    Add JS function for password config and generation

--- script.js
+++ script.js
@@ -102,3 +102,129 @@ copyIcon.addEventListener("click", function (ev) {
     document.execCommand("copy");
     window.getSelection().removeAllRanges();
 });
+
+const upperCaseCharacters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  
+  const lowerCaseCharacters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  
+  const numericCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  
+  const specialCharacters = [
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "|",
+    "?",
+    ".",
+    "~",
+  ];
+  
+  function generatePassword() {
+  let myPassword = [];
+  
+  while (myPassword.length !== passwordConfig.length) {
+    if (passwordConfig.uppercase) {
+      let randomNum = Math.floor(Math.random() * upperCaseCharacters.length);
+      myPassword.push(upperCaseCharacters[randomNum]);
+      if (myPassword.length === passwordConfig.length) {
+        break;
+      }
+    }
+    if (passwordConfig.lowercase) {
+      let randomNum = Math.floor(Math.random() * lowerCaseCharacters.length);
+      myPassword.push(lowerCaseCharacters[randomNum]);
+      if (myPassword.length === passwordConfig.length) {
+        break;
+      }
+    }
+    if (passwordConfig.numeric) {
+      let randomNum = Math.floor(Math.random() * numericCharacters.length);
+      myPassword.push(numericCharacters[randomNum]);
+      if (myPassword.length === passwordConfig.length) {
+        break;
+      }
+    }
+    if (passwordConfig.special) {
+      let randomNum = Math.floor(Math.random() * specialCharacters.length);
+      myPassword.push(specialCharacters[randomNum]);
+      if (myPassword.length === passwordConfig.length) {
+        break;
+      }
+    }
+    
+  }
+  
+  // randomize order of characters
+  
+  let shuffleNum = myPassword.length;
+  while (shuffleNum > 0) {
+    let randomIndex = Math.floor(Math.random() * shuffleNum);
+    shuffleNum = shuffleNum - 1;
+    let tempChar = myPassword[shuffleNum];
+    myPassword[shuffleNum] = myPassword[randomIndex];
+    myPassword[randomIndex] = tempChar;
+  }
+  
+  myPassword = myPassword.join("");
+  
+  return myPassword;
+  }
